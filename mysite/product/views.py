dc86dc92ada002264f523fb5c4bb74b5f2803b25
Igor20p05 <chernobay.i2112@gmail.com>
2023-10-25 10:09:22 +0300

    new view

--- mysite/product/views.py
+++ mysite/product/views.py
@@ -4,6 +4,7 @@ from django.views import generic
 from .models import Product
 
 class IndexView(generic.ListView):
+    model = Product
     template_name = "product/index.html"
     context_object_name = "product_list"
     
@@ -14,7 +15,3 @@ class DetailView(generic.DetailView):
     model = Product
     template_name = "product/detail.html"
     context_object_name = "product"
-    
-# def detail(request, product_id):
-#     product = get_object_or_404(Product, pk=product_id)
-#     return render(request, "product/detail.html", {"product": product})
