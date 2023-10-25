5e1e44e6fc8abbf5a97867b2fb6611ad015e0e30
Igor20p05 <chernobay.i2112@gmail.com>
2023-10-25 08:42:31 +0300

    add datail view, 24.10

--- mysite/product/urls.py
+++ mysite/product/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = "product"
 
 urlpatterns = [
+    # Главная страница
     path("", views.IndexView.as_view(), name="index"),
     # detail/pk_product
     path("detail/<int:pk>/", views.DetailView.as_view(), name="detail"),
