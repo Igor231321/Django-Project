a5979e88dd0968a5f90772424cebdd3e7ba77186
Igor20p05 <chernobay.i2112@gmail.com>
2023-11-22 20:58:24 +0200

    new pagination

--- mysite/product/urls.py
+++ mysite/product/urls.py
@@ -9,11 +9,11 @@ urlpatterns = [
     path("", views.index_page, name="index"),
     # product_detail
     # path("detail/<int:pk>/", views.DetailView.as_view(), name="detail"),
-    path("detail/<slug:product_slug>/", views.show_product, name="detail"),
+    path("detail/<slug:product_slug>/", views.detail_product, name="detail"),
     # show_all_product
-    path("all_product/", views.AllProductView.as_view(), name="all_product" ),
+    path("all_product/", views.product_list, name="all_product"),
     # show_category
-    path("category/<slug:category_slug>/", views.show_category, name="category"),
+    path("category/<slug:category_slug>/", views.detail_category, name="category"),
     # buy form
     path("detail/<slug:product_slug>/buy", views.buy_page, name="buy_menu"),
 ]
--- mysite/product/views.py
+++ mysite/product/views.py
@@ -1,58 +1,67 @@
 from django.views import generic
 from django.shortcuts import render, get_object_or_404
-
+from django.core.paginator import Paginator, EmptyPage
 from .models import Product, Category
-            
+
 # class IndexView(generic.ListView):
 #     """Выводит 3 карточки товара."""
 #     model = Product
 #     template_name = "product/index.html"
 #     context_object_name = "product_list"
+#     queryset = Product.published.all()[:3]
 
-#     def get_queryset(self):
-#         return Product.published.all()[:3]
 
 def index_page(request):
     products = Product.published.all()[:3]
-    context = {"product_list": products,}
-    
+    context = {
+        "product_list": products,
+    }
+
     return render(request, "product/index.html", context)
+
 
 # class DetailView(generic.DetailView):
 #     model = Product
 #     template_name = "product/detail.html"
 #     context_object_name = "product"
 
-def show_product(request, product_slug):
+
+def detail_product(request, product_slug):
     """Выводит один товар."""
     product = get_object_or_404(Product, slug=product_slug)
     return render(request, "product/detail.html", {"product": product})
 
-
-class AllProductView(generic.ListView):
-    """Выводит весь товар."""
-
-    model = Product
-    template_name = "product/all_product.html"
-    context_object_name = "product_list"
-
-    def get_queryset(self):
-        return Product.objects.all()
+# class ProductListView(generic.ListView):
+#     model = Product
+#     queryset = Product.published.all()
+#     context_object_name = 'products'
+#     paginate_by = 3
+#     template_name = 'product/all_product.html'
 
 
-def show_category(request, category_slug):
+def product_list(request):
+    product_list = Product.published.all()
+    paginator = Paginator(product_list, 2)
+    page_number = request.GET.get("page", 1)
+    try:
+        products = paginator.page(page_number)
+    except EmptyPage:
+        # if page_number has don't diaposon, get last page
+        products = paginator.page(paginator.num_pages)
+    return render(request, "product/all_product.html", {"products": products})
+
+
+def detail_category(request, category_slug):
     """Выводит одну категорию отобраною по короткой метке (Slug)."""
     category = get_object_or_404(Category, slug=category_slug)
     products = Product.published.filter(category_id=category)
-    
-    return render(request, "product/index.html", {"category": category, 
-                                                  "products": products})
+    return render(
+        request, "product/index.html", {"category": category, "products": products}
+    )
 
 
 def buy_page(request, product_slug):
     """Выводит форму по отдельному товару."""
     product = get_object_or_404(Product, slug=product_slug)
     content = {"product": product}
-    
     return render(request, "product/buy_product.html", content)
-    
